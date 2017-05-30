f1af29c835b644586a7c25644ae852b6fb8968b9
Allen Chen <alchen1231@gmail.com>
2017-05-30 01:02:02 -0700

    more logic

--- bundle.js
+++ bundle.js
@@ -70,6 +70,23 @@
 /* 0 */
 /***/ (function(module, exports) {
 
+// import Game from './game';
+
+document.addEventListener("DOMContentLoaded", function(){
+  const board = document.getElementById('tetris-board');
+  board.width = 200;
+  board.height = 380;
+  const ctx = board.getContext('2d');
+  ctx.fillStyle = 'rgb(44, 44, 42)';
+  ctx.fillRect(0, 0, 200, 380);
+  ctx.scale(20, 20);
+  // ctx.fillStyle = 'rgb(255, 51, 0)';
+  // ctx.fillRect(0,3,1,1);
+  // ctx.fillRect(10,3,1,1);
+  // ctx.fillRect(5,3,1,1);
+  // ctx.fillRect(4,2,1,1);
+  // const game = new Game();
+});
 
 
 /***/ })
