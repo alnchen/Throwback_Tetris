4c95a10ee5e022eba536779dd00690ab74503dfe
Allen Chen <alchen1231@gmail.com>
2017-05-30 11:02:18 -0700

    piece is moving

--- bundle.js
+++ bundle.js
@@ -63,30 +63,234 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-// import Game from './game';
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__board__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__piece__ = __webpack_require__(3);
+
+
+
+class Game {
+  constructor() {
+    this.board = new __WEBPACK_IMPORTED_MODULE_0__board__["a" /* default */](10, 19);
+    this.score = 0;
+    this.piece = new __WEBPACK_IMPORTED_MODULE_1__piece__["a" /* default */]();
+    // console.log(this.piece.createPiece());
+  }
+
+  drop() {
+    this.piece.pos.y += 1;
+    // console.log(this.piece.pos.y);
+  }
+
+
+
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Game);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Board {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.board = this.createBoard(this.width, this.height);
+  }
+
+  createBoard(width, height) {
+    const board = [];
+    for (var i = 0; i < height.length; i++) {
+      board.push(new Array(width).fill(0));
+    }
+    return board;
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Board);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__game__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__game_view__ = __webpack_require__(4);
+
+
 
 document.addEventListener("DOMContentLoaded", function(){
   const board = document.getElementById('tetris-board');
   board.width = 200;
   board.height = 380;
   const ctx = board.getContext('2d');
-  ctx.fillStyle = 'rgb(44, 44, 42)';
-  ctx.fillRect(0, 0, 200, 380);
   ctx.scale(20, 20);
-  // ctx.fillStyle = 'rgb(255, 51, 0)';
-  // ctx.fillRect(0,3,1,1);
-  // ctx.fillRect(10,3,1,1);
-  // ctx.fillRect(5,3,1,1);
-  // ctx.fillRect(4,2,1,1);
-  // const game = new Game();
+
+  const game = new __WEBPACK_IMPORTED_MODULE_0__game__["a" /* default */]();
+  const gameview = new __WEBPACK_IMPORTED_MODULE_1__game_view__["a" /* default */](ctx, game);
+  gameview.gameStart();
 });
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Piece {
+  constructor() {
+    this.pos = {
+      x: 4,
+      y: 0
+    };
+    this.shape = null;
+    this.createPiece();
+  }
+
+  // const COLORS = [tbd]
+
+  pieceShape(type)
+  {
+    switch(type) {
+    case 'I':
+      return [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ];
+    case 'L':
+      return [
+        [0, 2, 0],
+        [0, 2, 0],
+        [0, 2, 2],
+      ];
+    case 'J':
+      return [
+        [0, 3, 0],
+        [0, 3, 0],
+        [3, 3, 0],
+      ];
+    case 'O':
+      return [
+        [4, 4],
+        [4, 4],
+      ];
+    case 'Z':
+      return [
+        [5, 5, 0],
+        [0, 5, 5],
+        [0, 0, 0],
+      ];
+    case 'S':
+      return [
+        [0, 6, 6],
+        [6, 6, 0],
+        [0, 0, 0],
+      ];
+    case 'T':
+      return [
+        [0, 7, 0],
+        [7, 7, 7],
+        [0, 0, 0],
+      ];
+    }
+  }
+
+  createPiece() {
+    const shapes = 'ILJOZST';
+    this.shape = this.pieceShape(shapes[Math.floor(Math.random() * 7)]);
+  }
+
+  move(dir) {
+    this.pos.x += dir;
+  }
+
+
+  rotate() {
+    //need suggestions on handling rotation
+    //transpose?
+  }
+
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Piece);
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__game__ = __webpack_require__(0);
+
+
+class GameView {
+  constructor(ctx, game) {
+    this.game = game;
+    this.ctx = ctx;
+    this.board = game.board;
+    this.piece = game.piece;
+    this.timeFrame = 1000;
+    this.update = this.update.bind(this);
+    this.draw = this.draw.bind(this);
+
+    window.addEventListener('keydown', e => {
+      e.preventDefault();
+      switch(e.keyCode) {
+        case 37:
+        this.piece.move(-1);
+        this.draw();
+        break;
+        case 39:
+        this.piece.move(1);
+        this.draw();
+        break;
+      }
+    });
+
+  }
+
+  draw() {
+    this.ctx.fillStyle = 'rgb(44, 44, 42)';
+    this.ctx.fillRect(0, 0, 200, 380);
+
+    this.piece.shape.forEach((row, idx) => {
+      row.forEach((element, idx2) => {
+        if (element !== 0) {
+          this.ctx.fillStyle = 'rgb(255, 51, 0)';
+          this.ctx.fillRect(idx2 + this.piece.pos.x, idx + this.piece.pos.y, 1, 1);
+        }
+      });
+    });
+  }
+
+  update() {
+    this.draw();
+    this.game.drop();
+  }
+
+  gameStart() {
+    // setInterval for requestAnimationFrame every 1000ms or so
+    setInterval(this.update, 1000);
+  }
+
+
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (GameView);
 
 
 /***/ })
